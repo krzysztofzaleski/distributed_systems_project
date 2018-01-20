e1c54c5fcadc620e53cc1de0d4401253ea4d81a7
Krzysztof Załęski <krzysiek.zaleski93@gmail.com>
2018-01-20 17:02:34 +0100

    new files

---
+++ witam.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <cstdlib>
+
+int main (int argc, char*argv[]) {
+   // float x,y;
+    //x = atof(argv[0]); y = atof(argv[1]);
+    //if (y==0)
+	printf("Hello World!!!!");
+    //else printf("Wynik dzielenia: %.2f\n",(float)x/y);
+    return 0;
+}
