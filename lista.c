25e07f35fa64c9fcd1a55edb60f94d9b4bf525bf
Krzysztof Załęski <krzysiek.zaleski93@gmail.com>
2018-01-21 15:40:43 +0100

    listowanie katalogow

--- lista.c
+++ lista.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <cstdlib>
+#include <dirent.h> 
 
 int main (int argc, char*argv[]) {
-    float x,y;
-    x = atof(argv[0]); y = atof(argv[1]);
-    printf("Wynik mnożenia: %.2f\n",(float)x*y);
-    return 0;
+	int main(void) {
+		DIR *d;
+		struct dirent *dir;
+		d = opendir(".");
+		if (d) {
+			while ((dir = readdir(d)) != NULL) {
+				printf("%s\n", dir->d_name);
+			}
+			closedir(d);
+		}
+		return(0);
+	}
  }    
